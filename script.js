98d7043a2c9be1bd0b772c86bea6e66fcb4b5b7d
hrk <39041842+sealotus1996@users.noreply.github.com>
2024-02-23 18:11:54 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ var windowHeight = window.innerHeight || document.documentElement.clientHeight |
 document.documentElement.style.setProperty('--windowWidth', windowWidth + 'px');
 document.documentElement.style.setProperty('--windowHeight', windowHeight + 'px');
 
+// 입력된 텍스트 길이에 따라 해당 길이만큼의 회색 반환
+function getBackgroundColor(length) {
+    var grayValue = Math.min(length, 255); // 최대값은 255
+    return "rgb(213, 200, 193)"; // R, G, B 값 모두 동일하게 설정하여 회색 생성
+}
 
 
 //채팅방추가 버튼 후 액션
@@ -20,23 +25,24 @@ document.getElementById("addchatroomBtn").addEventListener("click", function() {
     document.getElementById("chatroomList").appendChild(newDiv);
 });
 
-//챗봇 인사말 helloMsg1에 대입
+//챗봇 인사말 helloMsg0에 대입
 document.addEventListener("DOMContentLoaded", function() {
-    // message1 변수에 텍스트를 저장
+    // message0 변수에 텍스트를 저장
     var message0 = "안녕하세요. ChitChatBot입니다. 처음 서비스를 이용하신다면 다음 질문들을 눌러 사용법을 알아보세요";
   
     // HTML 요소에 동적으로 텍스트를 삽입
-    document.getElementById('helloMsg1').textContent = message0;
+    document.getElementById('helloMsg0').textContent = message0;
+
   });
 
 // 첫번째 기본 질문 message1에 대입
 document.addEventListener("DOMContentLoaded", function() {
     // message1 변수에 텍스트를 저장
     var message1 = "ChitChatBot은 어떤 서비스야?";
-  
     // HTML 요소에 동적으로 텍스트를 삽입
     document.getElementById('messageElement1').textContent = message1;
-  });
+  
+});
 
 // 첫번째 기본 질문에 대한 답변
 document.getElementById("defaultMessageBtn1").addEventListener("click", function() {
@@ -115,10 +121,36 @@ document.getElementById("defaultMessageBtn3").addEventListener("click", function
     // .chatting-div 요소의 맨 아래에 새로운 채팅을 추가
     chattingDiv.appendChild(newChat);
 
-    //가장 하단으로 이동
-    var parentDiv = document.querySelector('.chatting-div');
-    parentDiv.scrollTop = parentDiv.scrollHeight;
+    //가장 하단(chattingDiv의 하단)으로 이동
+    chattingDiv.scrollTop = chattingDiv.scrollHeight;
 });
+
+// insertData 함수 정의
+function insertData() {
+    // 입력된 메시지 가져오기
+    var message = document.getElementById('inputUserMsg').value;
+
+    // 채팅창 요소 가져오기
+    var chattingDiv = document.querySelector('.chatting-div');
+
+    // 새로운 채팅 요소 생성
+    var newChat = document.createElement('div');
+    newChat.classList.add('my-chat');
+    newChat.innerHTML = `
+        <img class="chat-icon" alt="" src="./resources/ccb_img/user_icon.png" />
+        <p class="message">${message}</p>
+    `;
+
+    // 채팅창에 새로운 채팅 추가
+    chattingDiv.appendChild(newChat);
+
+    // 입력된 메시지 초기화
+    document.getElementById('inputUserMsg').value = '';
+
+    // 가장 하단으로 스크롤
+    chattingDiv.scrollTop = chattingDiv.scrollHeight;
+}
+
 
 
 
